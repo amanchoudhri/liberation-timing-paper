6357393425f111216ee9e5c59fcbd42aac3c98ad
aman527 <amanchoudhri@gmail.com>
2022-01-17 18:45:00 -0500

    Draft ventilator-free days code (untested)

--- common.py
+++ common.py
@@ -32,3 +32,41 @@ def group_by_stay(df: pd.DataFrame, col: str = None):
     Returns either the grouped dataframe or optionally, just one grouped column."""
     grouped = df.groupby('stay_id', axis='rows', group_keys=False)
     return grouped[col] if col else grouped
+
+def calibration_curve(
+    df: pd.DataFrame, 
+    p_hat: str, 
+    y: str, 
+    title: str, 
+    filename: str = ''
+) -> None:
+    """
+    Create, save, and display a calibration curve using the given propensity
+    scores and outcome variable.
+
+    Parameters:
+        df (pd.DataFrame): a dataframe storing the propensity score and the outcome
+        p_hat (str): name of propensity score column
+        y (str): name of outcome column
+        title (str): title of figure
+        filename (str, optional): if provided, path to which the fig will be saved.
+    """
+    prob_pred = []
+    prob_true = []
+
+    RESOLUTION = 0.02
+    MAX_X = 0.25
+    for i in range(int(MAX_X / RESOLUTION)):
+        prob_true.append(i * RESOLUTION)
+        rows = (df[p_hat] >= RESOLUTION * i) & (df[p_hat] <= RESOLUTION * (i + 1))
+        prob_pred.append(df.loc[rows, 'weaning'].mean())
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    ax.set_xlabel('Mean Predicted Probability')
+    ax.set_ylabel('Proportion of Positives')
+    ax.set_title(title)
+    ax.plot(prob_true, prob_pred)
+    ax.axline([0, 0], [0.2, 0.2], color='black', alpha=0.3)
+    if filename:
+        fig.savefig(f'images/{filename}', transparent=False)
+    plt.show()
